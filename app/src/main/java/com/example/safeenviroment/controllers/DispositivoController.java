63d64be50b4a7f4238f1dcd5e225eede5e466eb4
cristian <chiat.contact@gmail.com>
2024-10-04 09:56:42 -0300

    Class DispositivoController with his CRUD Methods DONE

---
+++ app/src/main/java/com/example/safeenviroment/controllers/DispositivoController.java
@@ -0,0 +1,63 @@
+package com.example.safeenviroment.controllers;
+
+import com.example.safeenviroment.models.Dispositivo;
+
+import java.util.ArrayList;
+
+public class DispositivoController {
+    private static final ArrayList<Dispositivo> dispositivoList = new ArrayList<>();
+
+    // CREATE
+    public static String addDispositivo(int id, float temperatura, float humedad, int bpm, float gasValue, int movimiento) {
+        try {
+            Dispositivo d = new Dispositivo();
+            d.setId(id);
+            d.setTemperatura(temperatura);
+            d.setHumedad(humedad);
+            d.setBpm(bpm);
+            d.setGasValue(gasValue);
+            d.setMovimiento(movimiento);
+            dispositivoList.add(d);
+            return "Dispositivo creado";
+        } catch (Exception e) {
+            return "Error:"+e.getMessage();
+        }
+    }
+
+    // READ
+    public static ArrayList<Dispositivo> findAll() {
+        return dispositivoList;
+    }
+    public static Dispositivo findDispositivo(int id) {
+        for (Dispositivo d : dispositivoList) {
+            if (d.getId() == id) {
+                return d;
+            }
+        }
+        return null;
+    }
+
+    // UPDATE
+    public static String editDispositivo(int id, float temperatura, float humedad, int bpm, float gasValue, int movimiento) {
+        Dispositivo d = findDispositivo(id);
+        if (d != null) {
+            d.setTemperatura(temperatura);
+            d.setHumedad(humedad);
+            d.setBpm(bpm);
+            d.setGasValue(gasValue);
+            d.setMovimiento(movimiento);
+            return "Dispositivo actualizado";
+        }
+        return "No existe el id";
+    }
+
+    // DELETE
+    public static boolean deleteDispositivo(int id) {
+        Dispositivo d = findDispositivo(id);
+        if (d != null) {
+            dispositivoList.remove(d);
+            return true;
+        }
+        return false;
+    }
+}
