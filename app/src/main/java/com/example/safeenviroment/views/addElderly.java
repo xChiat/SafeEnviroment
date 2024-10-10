a7f02ba820a51610b4df3e67bba9c39826636085
cristian <chiat.contact@gmail.com>
2024-10-10 11:02:57 -0300

    formulario para agregar ancianos

--- app/src/main/java/com/example/safeenviroment/views/addElderly.java
+++ app/src/main/java/com/example/safeenviroment/views/addElderly.java
@@ -1,6 +1,9 @@
 package com.example.safeenviroment.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +12,45 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.safeenviroment.R;
+import com.example.safeenviroment.controllers.ElderlyController;
 
 public class addElderly extends AppCompatActivity {
-
+    EditText name, rut, age, emergencyNumber, infoMed, infoAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_elderly);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        name = findViewById(R.id.etNombre);
+        rut = findViewById(R.id.etRut);
+        age = findViewById(R.id.etAge);
+        emergencyNumber = findViewById(R.id.etPhone);
+        infoMed = findViewById(R.id.etInfomed);
+        infoAdd = findViewById(R.id.etInfoad);
+
+    }
+    public void addElderly(View v) {
+        if (areFieldsNotEmpty(name, rut, age, emergencyNumber, infoMed, infoAdd)) {
+            ElderlyController.addElderly(
+                    rut.getText().toString(),
+                    name.getText().toString(),
+                    Integer.parseInt(age.getText().toString()),
+                    emergencyNumber.getText().toString(),
+                    infoMed.getText().toString(),
+                    infoAdd.getText().toString()
+            );
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        } else {
+            System.out.println("Error: Campos vacíos");
+        }
+    }
+    private boolean areFieldsNotEmpty(EditText... fields) {
+        for (EditText field : fields) {
+            if (field.getText().toString().trim().isEmpty()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
